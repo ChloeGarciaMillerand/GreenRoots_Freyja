daae6c5718d2d14adfeb4c835a3a5cf18c29d210
adam-benyekkou <adam.benyekkou.pro@hotmail.com>
2025-08-18 15:14:00 +0200

    fix - Adapted the file structure for React Router, and added working scripts
    Co-authored-by: ChloeGarciaMillerand <ChloeGarciaMillerand@users.noreply.github.com>
    Co-authored-by: VincentVautier <vincent.vautier@oclock.school>
    Co-authored-by: Adam Benyekkou <adam-benyekkou@users.noreply.github.com>
    Co-authored-by: JohanRozanOclock <JohanRozanOclock@users.noreply.github.com>
    Co-authored-by: Cédric FAMIBELLE-PRONZOLA <contact@cedric-pronzola.dev>

--- @front/src/main.tsx
+++
@@ -1,9 +0,0 @@
-import { createRoot } from 'react-dom/client'
-import { Home } from './app/features/home/Home'
-import './style.scss'
-
-
-const container = document.getElementById('app')!
-
-const root = createRoot(container)
-root.render(<Home />)
--- @front/vite.config.ts
+++ @front/vite.config.ts
@@ -1,8 +1,10 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import { reactRouter } from "@react-router/dev/vite";
+import tsconfigPaths from "vite-tsconfig-paths";
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-	plugins: [react()],
+	plugins: [react(), reactRouter(), tsconfigPaths()],
 	server: {
 		port: 3000,
 		proxy: {
