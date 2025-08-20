9551fad98758d067981097e907a69969098d1d39
adam-benyekkou <adam.benyekkou.pro@hotmail.com>
2025-08-20 18:09:10 +0200

    feat - Added localizationModel and changed localizationController to handle queries to the database - test is working Co-authored-by: ChloeGarciaMillerand <ChloeGarciaMillerand@users.noreply.github.com> Co-authored-by: VincentVautier <vincent.vautier@oclock.school> Co-authored-by: Adam Benyekkou <adam-benyekkou@users.noreply.github.com> Co-authored-by: JohanRozanOclock <JohanRozanOclock@users.noreply.github.com> Co-authored-by: Cédric FAMIBELLE-PRONZOLA <contact@cedric-pronzola.dev>

--- @back/src/routes/localizationRouter.ts
+++ @back/src/routes/localizationRouter.ts
@@ -1,9 +1,9 @@
 import { Router } from 'express';
-import { localizationsController } from "../app/Controllers/localizationsController.js";
+import { localizationController } from "../app/Controllers/localizationController.js";
 
 const localizationRouter = Router();
 
-localizationRouter.get('/api/locations', localizationsController.index);
-localizationRouter.get('/api/locations/:id', localizationsController.show);
+localizationRouter.get('/api/locations', localizationController.index);
+localizationRouter.get('/api/locations/:id', localizationController.show);
 
 export { localizationRouter };
