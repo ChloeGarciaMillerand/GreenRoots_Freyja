b981e3aceb04c9dc5ca6c6e0100d6096b50fde37
ChloeGarciaMillerand <chloe@garcia.cloud>
2025-08-21 10:18:43 +0200

    style(homePageTreeCard: prepare css

--- @front/app/pages/home/HomePageTreeCard/HomePageTreeCard.tsx
+++ @front/app/pages/home/HomePageTreeCard/HomePageTreeCard.tsx
@@ -11,12 +11,20 @@ function HomePageTreeCard({
 }: HomePageTreeCardProps) {
 	return (
 		<div className="tree-card">
-			<img src={image} alt={name} style={{ width: "100%", height: "auto" }} />
-			<h3>{name}</h3>
-			<p>Prix : {price} €</p>
-			<p>Localisation : {localization}</p>
-			<p>Projet : {project_name}</p>
-			<AddCartButton treeId={treeId} />
+			<img src={image} alt={name} />
+			<div className="tree-card-text-content">
+				<div className="tree-card-name-price">
+					<h3>{name}</h3>
+					<p>Prix : {price} €</p>
+				</div>
+				<div className="tree-card-localization-project">
+					<p>Localisation : {localization}</p>
+					<p>Projet : {project_name}</p>
+				</div>
+				<div className="tree-card-add-cart">
+					<AddCartButton treeId={treeId} />
+				</div>
+			</div>
 		</div>
 	);
 }
