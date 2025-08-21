ec9d1f35f4797202b80deff65f413bde5b0e66bf
ChloeGarciaMillerand <chloe@garcia.cloud>
2025-08-21 10:25:43 +0200

    feat(quantitySelector): add component

---
+++ @front/app/components/shared/components/quantitySelector/quantitySelector.tsx
@@ -0,0 +1,27 @@
+import { useState } from "react";
+
+export const QuantitySelector = () => {
+	const [count, setCount] = useState(1);
+
+	const increment = () => {
+		setCount(count + 1);
+	};
+
+	const decrement = () => {
+		if (count > 1) {
+			setCount(count - 1);
+		}
+	};
+
+	return (
+		<div className="quantity-selector">
+			<button type="button" onClick={decrement} disabled={count === 1}>
+				-
+			</button>
+			<span className="quantity-count">{count}</span>
+			<button type="button" onClick={increment}>
+				+
+			</button>
+		</div>
+	);
+};
