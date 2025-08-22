f0ecaa1e7cc3ece6b9f02ecae0b556c5338a49c7
Cédric FAMIBELLE-PRONZOLA <c.pronzola@tuta.io>
2025-08-22 18:36:34 +0400

    feat(login): create LoginForm component
    Co-authored-by: ChloeGarciaMillerand <ChloeGarciaMillerand@users.noreply.github.com>
    Co-authored-by: VincentVautier <vincent.vautier@oclock.school>
    Co-authored-by: Adam Benyekkou <adam-benyekkou@users.noreply.github.com>
    Co-authored-by: JohanRozanOclock <JohanRozanOclock@users.noreply.github.com>
    Co-authored-by: Cédric FAMIBELLE-PRONZOLA <contact@cedric-pronzola.dev>

--- @front/app/pages/login/Login.tsx
+++ @front/app/pages/login/Login.tsx
@@ -1,7 +1,10 @@
+import LoginForm from "~/components/features/forms/login/LoginForm";
+
 export default function Login() {
 	return (
 		<div>
 			<h1>Login Page</h1>
+      <LoginForm />
 		</div>
 	);
 }
