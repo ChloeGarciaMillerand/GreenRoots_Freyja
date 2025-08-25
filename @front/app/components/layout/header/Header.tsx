85f036bf85c609fbc7d6fb6a41d80cfc14b61224
ChloeGarciaMillerand <chloe@garcia.cloud>
2025-08-25 14:15:48 +0200

    fix(header): style sub menu ok

--- @front/app/components/layout/header/Header.tsx
+++ @front/app/components/layout/header/Header.tsx
@@ -1,13 +1,13 @@
 import "./Header.css";
 import NavbarMobile from "./navbarMobile/navbarMobile";
 import NavbarDesktop from "./navbarDesktop/navbarDesktop";
-import { Link } from "react-router";
+import { Form, Link } from "react-router";
 
 import logo from "../../../../assets/icons/logo.svg";
 import iconAccount from "../../../../assets/icons/iconAccount.svg";
 import iconCart from "../../../../assets/icons/iconCart.svg";
 
-export function Header() {
+export function Header(props: { user: any }) {
 	return (
 		<div>
 			<header>
@@ -27,9 +27,36 @@ export function Header() {
 					<Link to="/">
 						<img src={iconCart} alt="Panier" />
 					</Link>
-					<Link to="/">
-						<img src={iconAccount} alt="Compte personnel" />
-					</Link>
+					<ul className="navlinks">
+						<li>
+							<details>
+								<summary className="summary">
+									<img src={iconAccount} alt="Compte personnel" />
+								</summary>
+								{props.user ? (
+									<ul className="account-submenu">
+										<li>
+											<Link to="/">{props.user.email}</Link>
+										</li>
+										<li>
+											<Form method="post" action="/logout">
+												<button type="button">Se déconnecter</button>
+											</Form>
+										</li>
+									</ul>
+								) : (
+									<ul className="account-submenu">
+										<li>
+											<Link to="/login">Se connecter</Link>
+										</li>
+										<li>
+											<Link to="/register">S'inscrire</Link>
+										</li>
+									</ul>
+								)}
+							</details>
+						</li>
+					</ul>
 				</div>
 			</header>
 		</div>
