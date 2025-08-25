4ce2fd138f1f5fc5ee7ec5d1e8003187f388a336
ChloeGarciaMillerand <chloe@garcia.cloud>
2025-08-25 11:41:01 +0200

    style(login): add css login page

--- @front/app/components/features/forms/login/LoginForm.tsx
+++ @front/app/components/features/forms/login/LoginForm.tsx
@@ -1,18 +1,22 @@
 import { Form } from "react-router";
+import "./LoginForm.css";
 
 function LoginForm() {
-
 	return (
-		<Form method='POST'>
-			<div>
-				<label htmlFor="email">Email:</label>
-				<input id="email" name="email" required />
+		<Form method="POST">
+			<div className="form-container">
+				<div className="field">
+					<label htmlFor="email">Email:</label>
+					<input id="email" name="email" required />
+				</div>
+				<div className="field">
+					<label htmlFor="password">Mot de passe:</label>
+					<input type="password" id="password" name="password" required />
+				</div>
+				<button type="submit" className="button-connexion">
+					Se connecter
+				</button>
 			</div>
-			<div>
-				<label htmlFor="password">Password:</label>
-				<input type="password" id="password" name="password" required />
-			</div>
-			<button type="submit">Login</button>
 		</Form>
 	);
 }
--- @front/app/pages/login/Login.tsx
+++ @front/app/pages/login/Login.tsx
@@ -1,60 +1,52 @@
-import type { Route } from "./+types/Login"
+import type { Route } from "./+types/Login";
 import LoginForm from "~/components/features/forms/login/LoginForm";
-import {
-  getSession,
-  commitSession,
-} from "~/services/sessions.server";
+import { getSession, commitSession } from "~/services/sessions.server";
 import { redirect } from "react-router";
+import "./Login.css";
 
 const API_URL = import.meta.env.VITE_API_URL;
 
+export async function action({ request }: Route.ActionArgs) {
+	const session = await getSession(request.headers.get("Cookie"));
 
-export async function action({
-  request,
-}: Route.ActionArgs) {
+	const formData = await request.formData();
+	const email = formData.get("email");
 
-	const session = await getSession(
-    	request.headers.get("Cookie"),
-	);
-  
-  let formData = await request.formData();
-  const email = formData.get("email");
-  
-    const password = formData.get("password");
+	const password = formData.get("password");
 
-    const userData = { email, password };
+	const userData = { email, password };
 
-    const response = await fetch(`${API_URL}/user/login`, {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(userData),
-    });
+	const response = await fetch(`${API_URL}/user/login`, {
+		method: "POST",
+		headers: {
+			"Content-Type": "application/json",
+		},
+		body: JSON.stringify(userData),
+	});
 
-    if (!response.ok) {
-      console.error("Login failed");
-      return;
-    }
+	if (!response.ok) {
+		console.error("Login failed");
+		return;
+	}
 
-    const data = await response.json();
+	const data = await response.json();
 	const token = data.data.token;
 
 	session.set("token", token);
 
-  // Login succeeded, send them to the home page.
-  return redirect("/", {
-    headers: {
-      "Set-Cookie": await commitSession(session),
-    },
-  });
+	// Login succeeded, send them to the home page.
+	return redirect("/", {
+		headers: {
+			"Set-Cookie": await commitSession(session),
+		},
+	});
 }
 
 export default function Login() {
 	return (
-		<div>
-			<h1>Login Page</h1>
-      <LoginForm />
-		</div>
+		<main className="login-container">
+			<h1>Connexion</h1>
+			<LoginForm />
+		</main>
 	);
 }
